6c8405f4efc1c6e4f332c3a51c24c9981a859cd2
wanmouren <113223125+wanmouren-jacky@users.noreply.github.com>
2022-09-14 11:36:47 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cheeseburger.mk
+    $(LOCAL_DIR)/kscope_cheeseburger.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_cheeseburger-user \
-    lineage_cheeseburger-userdebug \
-    lineage_cheeseburger-eng
+    kscope_cheeseburger-user \
+    kscope_cheeseburger-userdebug \
+    kscope_cheeseburger-eng
